4f7b8789fc4240eac69991cd9e102041cad4595e
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2023-06-30 22:55:11 -0300

    chore: add tailwind colors variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,32 +11,19 @@ module.exports = {
             CINZA: '#f3f4f6',
             VERDE_CLARO: '#c3ffc9',
 
-            // WHITE: '#FDFFFF',
-            // DARK_WHITE: '#FDFFFF',
-            // LIGHT_GRAY: '#F6F6F6',
-            // MEDIUM_GRAY: '#D9D9D9',
-            // DARK_GRAY: '#646464',
-            // BLACK: '#222222',
-            // GREEN : '#00C44B',
-            // RED: '#F35757',
-            // LIGHT_BLUE: '#0FA0E3',
-            // DARK_BLUE: '#5045E6',
-            // LIGHT_YELLOW: '#FFCC4D',
-            // DARK_YELLOW: '#FFAC33',
+            PRINCIPAL: '#5C8A74',
+            PRINCIPAL_DARK: '232F24',
 
-            // DARK_THEME_TITLE: '#FFFFFF',
-            // DARK_THEME_TEXT: '#D9D9D9',
-            // DARK_THEME_BACKGROUND: '#1F1F1F',
-            // DARK_THEME_BACKGROUND_SECONDARY: '#000000',
-            // DARK_THEME_HOVER: "#11111F",
-            // DARK_THEME_HOVER_RED: "#ab1919",
+            SECONDARY: 'D97251',
 
-            // LIGHT_THEME_TITLE: '#222222',
-            // LIGHT_THEME_TEXT: '#646464',
-            // LIGHT_THEME_BACKGROUND: '#FFFFFF',
-            // LIGHT_THEME_BACKGROUND_SECONDARY: '#F6F6F6',
-            // LIGHT_THEME_HOVER: '#EFF6FF',
-            // LIGHT_THEME_HOVER_RED: '#374151'
+            WHITE_PRINCIPAL: '#F5F5F5',
+            WHITE_SECONDARY: '#EAEAEA',
+            WHITE_TERTIARY: '#DADADA',
+
+            GRAY:'#808080',
+            GRAY_SECONDARY: '595959',
+            GRAY_DARK: '#1C1C1C'
+
           }
         },
     },
