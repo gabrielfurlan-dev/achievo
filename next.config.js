0bd5f0e1315384f24270d451539c84e1d963eea7
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2023-08-15 12:04:22 -0300

    chore: desable eslint

--- next.config.js
+++ next.config.js
@@ -4,13 +4,14 @@
 // });
 
 module.exports = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
+    eslint: { ignoreDuringBuilds: true },
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
 };
