a1d5493c4701d683562a5da66a8a24ea3924ad13
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2023-08-15 15:30:39 -0300

    chore: change api url base

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,1 +1,10 @@
-export default "http://localhost:3000";
+
+export default function () {
+    if (process.env.NODE_ENV === "production") {
+        return "https://weeklyreport-preview.vercel.app";
+    }
+    else {
+        return "http://localhost:3000";
+    }
+}
+
