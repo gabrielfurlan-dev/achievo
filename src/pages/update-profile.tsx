dc2bbd73678b0f0cc8c170f22884737bfab30d38
Gabriel Furlan <gabriel.silva@inovafarma.com.br>
2023-09-01 00:05:44 -0300

    chore: add go back button

--- src/pages/update-profile.tsx
+++ src/pages/update-profile.tsx
@@ -1,12 +1,15 @@
+import { CompactNavBar } from "@/components/NavBar/CompactNavBar";
 import { SimpleNavBar } from "@/components/NavBar/SimpleNavBar";
 import PageLayout from "@/layouts/PageLayout";
 import { UpdateUserLayout } from "@/layouts/UpdateUserLayout";
+import { useUserInfoStore } from "@/store/userStoreInfo";
 import { AddressBook } from "phosphor-react";
 
 export default function pages() {
+    const { userInfo } = useUserInfoStore()
     return (
         <PageLayout>
-            <SimpleNavBar IconPage={AddressBook} title="Editar perfil" subTitle="" />
+            <CompactNavBar goBackUrl={"/profile/" + userInfo.id} IconPage={AddressBook} title="Editar perfil" subTitle="" />
             <UpdateUserLayout destinationPathOnUpdate="/home" isFinishingRegister />
         </PageLayout>
     );
