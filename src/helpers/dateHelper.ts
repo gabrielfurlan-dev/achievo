cc481be7c8f80d52efd10291e8f0ae3981ab6719
Gabriel Garcia <gokugamer190@gmail.com>
2023-09-04 21:31:42 -0300

    fix: Fix incorrect date in different countries

--- src/helpers/dateHelper.ts
+++ src/helpers/dateHelper.ts
@@ -1,4 +1,4 @@
-import { format, parseISO } from "date-fns";
+import { format } from "date-fns";
 
 export function getCurrentDate() {
     const currentDate = new Date();
@@ -13,7 +13,7 @@ export function getCurrentDate() {
         day = "0" + day;
     }
 
-    return `${year}-${month}-${day}`;
+    return `${month}-${month}-${day}`;
 }
 
 export function getWeekInterval(date: Date) {
@@ -30,15 +30,24 @@ export function getWeekInterval(date: Date) {
 
 export function getFormatedWeekInterval(date: string) {
     const interval = getWeekInterval(stringToDate(date));
-    return `${interval.firstDayOfWeek
-        .toLocaleDateString()
-        .substring(0, 5)} até ${interval.lastDayOfWeek
-        .toLocaleDateString()
-        .substring(0, 5)} de ${interval.lastDayOfWeek.getFullYear()}`;
+    const userLocale = navigator.language;
+
+    if (userLocale.toUpperCase() == "PT-BR") {
+        const firstDayFormatted = `${interval.firstDayOfWeek.getDate().toString().padStart(2, '0')}/${(interval.firstDayOfWeek.getMonth() + 1).toString().padStart(2, '0')}`;
+        const lastDayFormatted = `${interval.lastDayOfWeek.getDate().toString().padStart(2, '0')}/${(interval.lastDayOfWeek.getMonth() + 1).toString().padStart(2, '0')}`;
+    
+        return `${firstDayFormatted} até ${lastDayFormatted} de ${interval.lastDayOfWeek.getFullYear()}`
+    }
+
+    const firstDayFormatted = `${(interval.firstDayOfWeek.getMonth() + 1).toString().padStart(2, '0')}/${interval.firstDayOfWeek.getDate().toString().padStart(2, '0')}`;
+    const lastDayFormatted = `${(interval.lastDayOfWeek.getMonth() + 1).toString().padStart(2, '0')}/${interval.lastDayOfWeek.getDate().toString().padStart(2, '0')}`;
+
+    return `${firstDayFormatted} até ${lastDayFormatted} de ${interval.lastDayOfWeek.getFullYear()}`;
 }
 
-export function getFormatedDate(date: string) {
-    return format(parseISO(date), "dd/MM/yyyy");
+function getFormatedDate(originalDate: Date) {
+    const formattedDate = format(originalDate, "MM/dd/yyyy");
+    return new Date(formattedDate);
 }
 
 export function stringToDate(date: string) {
@@ -46,5 +55,8 @@ export function stringToDate(date: string) {
     const year = parseInt(parts[0], 10);
     const month = parseInt(parts[1], 10) - 1;
     const day = parseInt(parts[2], 10);
-    return new Date(year, month, day);
+
+    const dateReport = new Date(year, month, day);
+
+    return new Date(getFormatedDate(dateReport));
 }
