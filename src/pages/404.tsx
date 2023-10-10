678e38a934b5563390d57ee6900a44a1d325db7c
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2023-10-10 12:35:55 -0300

    fix: report exists validation

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,21 +7,15 @@ export default function error404() {
     return (
         <div className="flex flex-col justify-between items-center m-auto">
 
-            <div className="">
-                <Ovni />
-            </div>
+            <div className=""><Ovni /></div>
 
             <div className="absolute flex top-80">
-
                 <div>
                     <span className="text-5xl font-bold">Ei capitão!</span>
                     <p className="w-64 text-lg text-NEUTRAL_300">Parece que a página que você está acessando foi abdusida.</p>
                 </div>
-
                 <Error />
-
                 <p className="w-64 text-lg text-NEUTRAL_300">Volte para a <Link className="underline text-NEUTRAL_100" href={"/home"}>página inicial</Link>.</p>
-
             </div>
 
             <div className="m-[-50px]">
