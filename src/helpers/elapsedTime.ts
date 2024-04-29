e4dc841df100c747380192bbbda86baa6cde2c71
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2024-04-29 19:39:32 -0300

    refactor: change function parameters

--- src/helpers/elapsedTime.ts
+++ src/helpers/elapsedTime.ts
@@ -1,4 +1,4 @@
-function getTimeElapsed(dataAtualizacao: string, typeDate: string): string {
+function getTimeElapsed(dataAtualizacao: string): string {
     const now = new Date();
     const updateDate = new Date(dataAtualizacao);
     const differenceMilliseconds = now.getTime() - updateDate.getTime();
@@ -6,41 +6,38 @@ function getTimeElapsed(dataAtualizacao: string, typeDate: string): string {
 
     if (seconds < 60) {
         return seconds < 5
-            ? `${typeDate} just now`
-            : `${typeDate} ${seconds} second${seconds !== 1 ? "s" : ""} ago`;
+            ? `just now`
+            : `${seconds} second${seconds !== 1 ? "s" : ""} ago`;
     }
 
     const minutes = Math.floor(seconds / 60);
     if (minutes < 60) {
-        return `${typeDate} ${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+        return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
     }
 
     const hours = Math.floor(minutes / 60);
     if (hours < 24) {
-        return `${typeDate} ${hours} hour${hours !== 1 ? "s" : ""} ago`;
+        return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
     }
 
     const days = Math.floor(hours / 24);
     if (days < 30) {
-        return `${typeDate} ${days} day${days !== 1 ? "s" : ""} ago`;
+        return `${days} day${days !== 1 ? "s" : ""} ago`;
     }
 
     const months = Math.floor(days / 30);
     if (months < 12) {
-        return `${typeDate} ${months} month${months !== 1 ? "s" : ""} ago`;
+        return `${months} month${months !== 1 ? "s" : ""} ago`;
     }
 
     const anos = Math.floor(months / 12);
-    return `${typeDate} ${anos} year${anos !== 1 ? "s" : ""} ago`;
+    return `${anos} year${anos !== 1 ? "s" : ""} ago`;
 }
 
-export function getCreatedTimeElapsed(
-    createdDate: string,
-    message: string = "Created"
-): string {
-    return getTimeElapsed(createdDate, message);
+export function getCreatedTimeElapsed(createdDate: string): string {
+    return `Created ${getTimeElapsed(createdDate)}`;
 }
 
 export function getUpdatedTimeElapsed(updatedDate: string): string {
-    return getTimeElapsed(updatedDate, "Updated");
+    return `Updated ${getTimeElapsed(updatedDate)}`;
 }
