032dec37bdb8436acc355630eceb1caa59b3e0c4
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2024-04-29 19:39:09 -0300

    fix: unit tests

--- test/elapsedTime.test.ts
+++ test/elapsedTime.test.ts
@@ -8,36 +8,36 @@ test("Should return updated elapsed time", () => {
     currentDate.setDate(currentDate.getDate() - 3);
 
     let result = getUpdatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Atualizado há 3 dias atrás`);
+    expect(result).toBe(`Updated 3 days ago`);
 
     currentDate = new Date();
     result = getUpdatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Atualizado agora mesmo`);
+    expect(result).toBe(`Updated just now`);
 
     currentDate = new Date();
     currentDate.setHours(currentDate.getHours() - 2);
     result = getUpdatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Atualizado há 2 horas atrás`);
+    expect(result).toBe(`Updated 2 hours ago`);
 
     currentDate = new Date();
     currentDate.setMinutes(currentDate.getMinutes() - 30);
     result = getUpdatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Atualizado há 30 minutos atrás`);
+    expect(result).toBe(`Updated 30 minutes ago`);
 
     currentDate = new Date();
     currentDate.setSeconds(currentDate.getSeconds() - 15);
     result = getUpdatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Atualizado há 15 segundos atrás`);
+    expect(result).toBe(`Updated 15 seconds ago`);
 
     currentDate = new Date();
     currentDate.setMonth(currentDate.getMonth() - 6);
     result = getUpdatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Atualizado há 6 mêses atrás`);
+    expect(result).toBe(`Updated 6 months ago`);
 
     currentDate = new Date();
     currentDate.setFullYear(currentDate.getFullYear() - 1);
     result = getUpdatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Atualizado há 1 ano atrás`);
+    expect(result).toBe(`Updated 1 year ago`);
 });
 
 test("Should return created elapsed time", () => {
@@ -45,34 +45,34 @@ test("Should return created elapsed time", () => {
     currentDate.setDate(currentDate.getDate() - 3);
 
     let result = getCreatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Criado há 3 dias atrás`);
+    expect(result).toBe(`Created 3 days ago`);
 
     currentDate = new Date();
     result = getCreatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Criado agora mesmo`);
+    expect(result).toBe(`Created just now`);
 
     currentDate = new Date();
     currentDate.setHours(currentDate.getHours() - 2);
     result = getCreatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Criado há 2 horas atrás`);
+    expect(result).toBe(`Created 2 hours ago`);
 
     currentDate = new Date();
     currentDate.setMinutes(currentDate.getMinutes() - 30);
     result = getCreatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Criado há 30 minutos atrás`);
+    expect(result).toBe(`Created 30 minutes ago`);
 
     currentDate = new Date();
     currentDate.setSeconds(currentDate.getSeconds() - 15);
     result = getCreatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Criado há 15 segundos atrás`);
+    expect(result).toBe(`Created 15 seconds ago`);
 
     currentDate = new Date();
     currentDate.setMonth(currentDate.getMonth() - 6);
     result = getCreatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Criado há 6 mêses atrás`);
+    expect(result).toBe(`Created 6 months ago`);
 
     currentDate = new Date();
     currentDate.setFullYear(currentDate.getFullYear() - 1);
     result = getCreatedTimeElapsed(currentDate.toISOString());
-    expect(result).toBe(`Criado há 1 ano atrás`);
+    expect(result).toBe(`Created 1 year ago`);
 });
